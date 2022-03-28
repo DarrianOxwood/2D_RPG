9741064c3bc6ade770c1b2f63c62a82ed395c162
Иван Ступко <DarrianCrashOxwood@yandex.ru>
2022-03-28 19:09:59 +0700

    Revert "Revert "attack system""
    This reverts commit 088c3eb5f09d3db95e88b05b4a257c2545a11b60.

---
+++ 2DRPG/Assets/Scripts/Components/ArmHeroComponent.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Scripts.Components
+{
+    public class ArmHeroComponent : MonoBehaviour
+    {
+
+        public void ArmHero(GameObject go)
+        {
+            var hero = go.GetComponent<Hero>();
+            if(hero != null)
+            {
+                hero.ArmHero();
+            }
+        }
+    }
+}
